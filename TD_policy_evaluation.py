a0140642acd5d01be2b3f53b629f1a37649138ea
Paulo-Rocha-97 <57599652+Paulo-Rocha-97@users.noreply.github.com>
2019-12-15 18:10:42 +0000

    *Update

---
+++ TD_policy_evaluation.py
@@ -0,0 +1,91 @@
+import os
+import gym
+import shutil 
+import scipy.io as sio
+from tqdm import tqdm
+from collections import defaultdict
+
+env=gym.make('Blackjack-v0')
+
+#%%########################################################################### 
+
+def policy(state):
+    
+    if state[0] > 19:
+        action = 0
+    else:
+        action = 1
+    return action
+
+#%%########################################################################### 
+
+def game_step(action):
+    
+    state_linha, reward, done, _ = env.step(action)
+    
+    return state_linha, reward, done 
+
+#%%########################################################################### 
+
+def convert_to_matlab(V):
+    
+    V_ = defaultdict(float)
+
+    for key in V.keys():
+        
+        Key_str = str(key)
+        
+        V_[Key_str] = V[key]
+        
+    return V_
+
+#%%########################################################################### 
+
+def td_0(env,num,alpha,gamma):
+    
+    V = defaultdict(float)
+
+    for i in tqdm(range(num)):
+        
+        S_t = env.reset()
+        
+        done = False
+        
+        while not done:
+            
+            action = policy(S_t)  
+            
+            S_t_1, R_t , done = game_step(action)
+
+            if done:
+                V[S_t_1] = float(0) 
+
+            V[S_t] = V[S_t] + alpha*(R_t + gamma*V[S_t_1] - V[S_t])
+            
+            S_t = S_t_1
+            
+    return V
+            
+#%%########################################################################### 
+
+num=1
+
+gamma=0.5
+
+alpha=0
+
+V = td_0(env,int(num*1000000),alpha,gamma)
+
+Data={}
+
+V_=convert_to_matlab(V)
+
+Data['V'] = dict(V_)
+    
+filename = f'{num}_TD_eva_a_{alpha}_g_{gamma}.mat'
+
+dr = os.getcwd() +'\Matlab\Dados'
+
+sio.savemat(filename,Data)
+
+shutil.move(filename,dr)
